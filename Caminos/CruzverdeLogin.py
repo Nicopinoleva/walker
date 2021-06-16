fecd969a8de02e17d513a40b3ea8fdc934c9240f
Nicolas Pino <nicolas.pinol@mail.udp.cl>
2021-06-16 18:54:42 -0400

    Removed captcha from Cruzverde

--- Caminos/CruzverdeLogin.py
+++ Caminos/CruzverdeLogin.py
@@ -22,7 +22,7 @@ obj.PORTAL = "CRUZVERDE"
 obj.passid = "password"
 obj.login_verify = True
 obj.enable_newBBR = True
-obj.enable_recaptcha = True
-obj.site_key = "6Le6POkUAAAAAPrhWc5b14fntw6TCU1tRgEKaLnk"
+# obj.enable_recaptcha = True
+# obj.site_key = "6Le6POkUAAAAAPrhWc5b14fntw6TCU1tRgEKaLnk"
 obj.account_procedure = account_special
 obj.run()
